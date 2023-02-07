6fb4c120bc1753d7674c0224da5aa62d901530b6
skpanagiotis <panagiotis.skias@gmail.com>
2023-02-07 23:38:09 +0200

    Update appModule and export dashboard as webComponent

--- projectOne/src/app/app.module.ts
+++ projectOne/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DashboardComponent],
+  imports: [BrowserModule, CommonModule, MatTableModule, MatPaginatorModule],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent],
+  entryComponents: [AppComponent, DashboardComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const element = createCustomElement(AppComponent, {
+      injector: this.injector,
+    });
+    customElements.define('app-project-one', element);
+
+    const element_dashboard = createCustomElement(DashboardComponent, {
+      injector: this.injector,
+    });
+    customElements.define('app-project-one-dashboard', element_dashboard);
+  }
+}
