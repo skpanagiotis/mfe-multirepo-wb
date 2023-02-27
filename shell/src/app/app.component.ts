0ca564209af172dfff1754385c1e3cdf79adb955
skpanagiotis <panagiotis.skias@gmail.com>
2023-02-27 23:32:26 +0200

    Update Shell

--- shell/src/app/app.component.ts
+++ shell/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'shell';
+  urlProject1 = 'http://localhost:8070/project-one/project-one.js';
+  urlProject2 = 'http://localhost:8080/project-two/project-two.js';
 }
